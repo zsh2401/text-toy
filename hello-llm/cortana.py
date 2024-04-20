5bbc12bf2c3ff0e1d5c2896dfb4285c9705394dc
zsh2401 <zsh2401@163.com>
2024-04-20 23:58:23 +0800

    更新HELLO LLM

---
+++ hello-llm/cortana.py
@@ -0,0 +1,30 @@
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
+
+def generate_text(prompt_text, model_name='gpt2', max_length=50):
+    
+    # 加载预训练模型及其分词器
+    tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+    model = GPT2LMHeadModel.from_pretrained(model_name)
+
+    # 编码输入文本，增加必要的格式标记
+    encoded_input = tokenizer.encode(prompt_text, return_tensors='pt')
+    
+    # 生成文本
+    output_sequences = model.generate(
+        input_ids=encoded_input,
+        max_length=max_length,
+        num_return_sequences=1,
+        no_repeat_ngram_size=2,
+        repetition_penalty=1.5,
+        top_p=0.92,
+        temperature=0.85,
+    )
+
+    # 解码生成的文本
+    return tokenizer.decode(output_sequences[0], skip_special_tokens=True)
+
+
+
+while(True):
+    
+    print(generate_text(input()))
